ec41957cfa8d5cbbd0fcb89788a6644a2e6044dd
Raphael Cabral <brcmesquita@gmail.com>
2023-07-08 19:54:20 -0300

    add página inicial

---
+++ resources/js/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 offset-md-2">
+        <h1>Bem-vindo ao meu projeto!</h1>
+        <p>Esta é a tela inicial.</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+}
+</script>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,8 @@
+<body>
+  <!-- Conteúdo da página -->
+
+  @yield('content')
+
+  <!-- Scripts -->
+  <script src="{{ mix('js/app.js') }}"></script>
+</body>
