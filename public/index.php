82d13c93723dc213bc76f42a0347cb56ca517d78
Raphael Cabral <brcmesquita@gmail.com>
2023-07-11 18:16:54 -0300

    updates

--- public/index.php
+++ public/index.php
@@ -1,30 +1,26 @@
 <?php
 session_start();
 
-$routes = require_once '../config/routes.php';
-require_once '../app/controllers/TodoController.php';
-require_once '../app/models/TodoModel.php';
-$config = require_once '../config/database.php';
-
-$pdo = new PDO(
-  'mysql:host='
-  . $config['host']
-  . ';dbname='
-  . $config['dbname'],
-  $config['username'],
-  $config['password']
-);
-
-$model = new TodoModel($pdo);
-$controller = new TodoController($model);
+require_once '../src/config/database.php';
+require_once '../src/routes/routes.php';
 
 $path = $_SERVER['REQUEST_URI'];
+
 if (array_key_exists($path, $routes)) {
   $action = $routes[$path];
-  $controller->$action(); 
+  require_once '../src/app/controllers/' . $action . '_controller.php';
+  $controllerClassName = ucfirst($action) . 'Controller';
+  $controller = new $controllerClassName();
+
+  if (method_exists($controller, $action)) {
+    $controller->{$action}();
+  } else {
+    header('HTTP/1.1 404 Not Found');
+    include '../src/app/views/404_view.php';
+    exit;
+  }
 } else {
   header('HTTP/1.1 404 Not Found');
-  include '../views/404.php';
+  include '../src/app/views/404_view.php';
   exit;
 }
-
