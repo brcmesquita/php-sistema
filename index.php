eec14db0f5127c4ec93e76893c5346d8794109cb
Raphael Cabral <brcmesquita@gmail.com>
2023-07-14 10:38:08 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,83 +1,40 @@
-<?php  
+<?php
 
-include_once 'model/Conexao.class.php';
-include_once 'model/Manager.class.php';
+// Inicia a sessão
+session_start();
 
-$manager = new Manager();
+// Verifica se o usuário está logado
+// if (!isset($_SESSION['user_id'])) {
+//     // Redireciona para a página inicial caso não esteja logado
+//     header('Location: index.php');
+//     exit;
+// }
+
+// Obtém o nome do usuário da sessão
+$nome = '';
+if (isset($_SESSION['user_id'])) {
+    $nome = $_SESSION['nome'];
+}
 
 ?>
+
 <!DOCTYPE html>
-<html>
+<html lang="pt-BR">
+
 <head>
-	<?php include_once 'view/dependencias.php'; ?>
+  <?php include_once "view/dependencias.php"; ?>
 </head>
+
 <body>
+  <?php include_once "view/menu.php"; ?>
 
-<div class="container">
-	
-	<h2 class="text-center">
-		List of Clients <i class="fa fa-list"></i>
-	</h2>
-
-	<h5 class="text-right">
-		<a href="view/page_register.php" class="btn btn-primary btn-xs">
-			<i class="fa fa-user-plus"></i>
-		</a>
-	</h5>
-
-	<div class="table-responsive">
-		<table class="table table-hover">
-			<thead class="thead">
-				<tr>
-					<th>ID</th>
-					<th>NOME</th>
-					<th>E-MAIL</th>
-					<th>CPF</th>
-					<th>DT. DE NASCIMENTO</th>
-					<th>ENDEREÇO</th>
-					<th>TELEFONE</th>
-					<th colspan="3">AÇÕES</th>
-				</tr>
-			</thead>
-			<tbody>
-				<?php foreach($manager->listClient("registros") as $client): ?>
-				<tr>
-					<td><?php echo $client['id']; ?></td>
-					<td><?php echo $client['name']; ?></td>
-					<td><?php echo $client['email']; ?></td>
-					<td><?php echo $client['cpf']; ?></td>
-					<td><?php echo $client['birth']; ?></td>
-					<td><?php echo $client['address']; ?></td>
-					<td><?php echo $client['phone']; ?></td>
-					<td>
-						<form method="POST" action="view/page_update.php">
-							
-							<input type="hidden" name="id" value="<?=$client['id']?>">
-
-							<button class="btn btn-warning btn-xs">
-								<i class="fa fa-user-edit"></i>
-							</button>
-
-						</form>
-					</td>
-					<td>
-						<form method="POST" action="controller/delete_client.php" onclick="return confirm('Você tem certeza que deseja excluir ?');">
-							
-							<input type="hidden" name="id" value="<?=$client['id']?>">
-
-							<button class="btn btn-danger btn-xs">
-								<i class="fa fa-trash"></i>
-							</button>
-
-						</form>
-					</td>
-				</tr>
-				<?php endforeach; ?>
-			</tbody>
-		</table>
-	</div>
-
-</div>
+  <div class="container mt-5">
+    <?php if ($nome) : ?>
+    <h1>Bem-vindo, <?php echo $nome; ?>!</h1>
+    <?php endif ?>
+    <p>Seja bem-vindo ao Sistema Web Pessoal.</p>
+  </div>
 
 </body>
+
 </html>
