fc69d713841ab34332faae9e86d9e4370f551543
Raphael Cabral <brcmesquita@gmail.com>
2023-07-14 10:40:01 -0300

    move file

---
+++ view/dependencias.php
@@ -0,0 +1,21 @@
+<!-- Required meta tags -->
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Titulo e Icone - HEAD -->
+<title>Sistema Web Pessoal</title>
+
+<link rel="shortcut icon" href="images/icon.png" type="image/x-icon">
+
+<!-- CDN's -->
+
+<!-- Bootstrap CSS v5.2.1 -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+<!-- Fontawesome 5 -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+
+<!-- GoogleFonts - OpenSans -->
+<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+
+<!-- Folha de Estilo(CSS) - Direta -->
